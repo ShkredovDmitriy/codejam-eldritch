40c04556f68b1089ee711b173b5c290e1b36dbd9
Dmitriy <shkredovdmitriy@gmail.com>
2022-08-23 00:57:23 +0300

    feat: add ancient cards, level buttons

--- src/app/modules/game/index.js
+++ src/app/modules/game/index.js
@@ -1,28 +1,94 @@
 import config from "./config";
-import { qs } from '../helpers/helpers';
+import { qi, qs, qa } from '../helpers/helpers';
 
-const game = () => {
+const game = async () => {
   console.log("START")
-  loading()
-
+  await loading();
+  showAncient();
+  selectAncient();
+  selectLevel()
 }
 
 // HELPERS
+function selectLevel () {
+  qi('levelVeryEasy').addEventListener('click', ()=> {
+    qa('.level__button').forEach(el => el.classList.remove('active'));
+    qi('levelVeryEasy').classList.add('active')
+  })
+  qi('levelEasy').addEventListener('click', ()=> {
+    qa('.level__button').forEach(el => el.classList.remove('active'));
+    qi('levelEasy').classList.add('active')
+  })
+  qi('levelNormal').addEventListener('click', ()=> {
+    qa('.level__button').forEach(el => el.classList.remove('active'));
+    qi('levelNormal').classList.add('active')
+  })
+  qi('levelHard').addEventListener('click', ()=> {
+    qa('.level__button').forEach(el => el.classList.remove('active'));
+    qi('levelHard').classList.add('active')
+  })
+  qi('levelVeryHard').addEventListener('click', ()=> {
+    qa('.level__button').forEach(el => el.classList.remove('active'));
+    qi('levelVeryHard').classList.add('active')
+  })
+}
+
+function showLevels(){
+  qi('levelVeryEasy').classList.add('visible');
+  setTimeout(() => qi('levelEasy').classList.add('visible'), 300);
+  setTimeout(() => qi('levelNormal').classList.add('visible'), 600);
+  setTimeout(() => qi('levelHard').classList.add('visible'), 900)
+  setTimeout(() => qi('levelVeryHard').classList.add('visible'), 1200);
+}
+
+function selectAncient() {
+  qi('azathoth').addEventListener("click", () => {
+    qa('.button__ancient').forEach(el => el.classList.remove('active'));
+    qi('azathoth').classList.add('active');
+    showLevels();
+  })
+  qi('cthulthu').addEventListener("click", () => {
+    qa('.button__ancient').forEach(el => el.classList.remove('active'));
+    qi('cthulthu').classList.add('active');
+    showLevels();
+  })
+  qi('iogSothoth').addEventListener("click", () => {
+    qa('.button__ancient').forEach(el => el.classList.remove('active'));
+    qi('iogSothoth').classList.add('active');
+    showLevels();
+  })
+  qi('shubNiggurath').addEventListener("click", () => {
+    qa('.button__ancient').forEach(el => el.classList.remove('active'));
+    qi('shubNiggurath').classList.add('active');
+    showLevels();
+  })
+}
+
+function showAncient() {
+  setTimeout(()=> qi('azathoth').classList.add('visible'), 1000);
+  setTimeout(()=> qi('cthulthu').classList.add('visible'), 1500)
+  setTimeout(()=> qi('iogSothoth').classList.add('visible'), 2000)
+  setTimeout(()=> qi('shubNiggurath').classList.add('visible'), 2500)
+}
+
 function loading() {
   let load = 0;
-  config.images.map(src => {
-    const img = new Image();
-    img.addEventListener('load', () => {
-      setTimeout(()=> {
-        load += 2;
-        if(load < 100) qs('.preloader__counter').textContent = load + '%';
-        if(load >= 100) {
-          qs('.preloader__counter').textContent = '100%'
-          loaderHide()
-        }
-      }, 500)
+  return new Promise((resolve, reject) => {
+    config.images.map(src => {
+      const img = new Image();
+      img.addEventListener('load', () => {
+        setTimeout(()=> {
+          load += 2;
+          if(load < 100) qs('.preloader__counter').textContent = load + '%';
+          if(load >= 100) {
+            qs('.preloader__counter').textContent = '100%'
+            loaderHide()
+            resolve()
+          }
+        }, 500)
+      })
+      img.src = './images/' + src;
     })
-    img.src = './images/' + src;
   })
 }
 
--- src/app/modules/helpers/helpers.js
+++ src/app/modules/helpers/helpers.js
@@ -1,1 +1,3 @@
+export const qi = (id) => document.getElementById(id)
 export const qs = (el) => document.querySelector(el)
+export const qa = (id) => document.querySelectorAll(id)
