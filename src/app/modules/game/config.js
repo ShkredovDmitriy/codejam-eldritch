d75a3372b47d956b6c0b52340d0d327761d06a26
Dmitriy <shkredovdmitriy@gmail.com>
2022-08-23 16:11:41 +0300

    feat: add functions for suffle cards and create game set

--- src/app/modules/game/config.js
+++ src/app/modules/game/config.js
@@ -1,71 +1,122 @@
-const config = {
-
-  images: [
-    // BACKGROUND
-    'background-desktop.jpg',
-    // BLUE CARDS
-    'blue1.png',
-    'blue2.png',
+const blueCards = {
+  easy: [
     'blue3.png',
     'blue4.png',
     'blue5.png',
-    'blue6.png',
-    'blue7.png',
-    'blue8.png',
-    'blue9.png',
     'blue10.png',
+  ],
+  normal: [
+    'blue7.png',
+    'blue9.png',
     'blue11.png',
     'blue12.png',
-    // BROWN CARDS
+  ], hard: [
+    'blue1.png',
+    'blue2.png',
+    'blue6.png',
+    'blue8.png'
+  ]
+};
+
+const brownCards = {
+  easy: [
+    'brown11.png',
+    'brown12.png',
+    'brown13.png',
+    'brown14.png',
+    'brown21.png',
+  ],
+  normal: [
     'brown1.png',
     'brown2.png',
     'brown3.png',
     'brown4.png',
     'brown5.png',
-    'brown6.png',
-    'brown7.png',
-    'brown8.png',
-    'brown9.png',
-    'brown10.png',
-    'brown11.png',
-    'brown12.png',
-    'brown13.png',
-    'brown14.png',
     'brown15.png',
     'brown16.png',
     'brown17.png',
     'brown18.png',
     'brown19.png',
     'brown20.png',
-    'brown21.png',
-    // GREEN CARDS
+  ], hard: [
+    'brown6.png',
+    'brown7.png',
+    'brown8.png',
+    'brown9.png',
+    'brown10.png',
+  ]
+};
+
+const greenCards = {
+  easy: [
     'green1.png',
-    'green2.png',
-    'green3.png',
-    'green4.png',
-    'green5.png',
-    'green6.png',
+    'green12.png',
+    'green16.png',
+    'green17.png',
+    'green18.png',
+  ],
+  normal: [
     'green7.png',
     'green8.png',
     'green9.png',
     'green10.png',
     'green11.png',
-    'green12.png',
     'green13.png',
     'green14.png',
     'green15.png',
-    'green16.png',
-    'green17.png',
-    'green18.png',
+
+  ], hard: [
+    'green2.png',
+    'green3.png',
+    'green4.png',
+    'green5.png',
+    'green6.png',
+  ]
+};
+
+const config = {
+  antients: {
+    azathoth: {
+      set: [[1, 2, 2], [2, 3, 4], [1, 1, 0]]
+    },
+    cthulthu: {
+      set: [[0, 1, 3], [2, 3, 4], [2, 0, 0]]
+    },
+    iogSothoth: {
+      set:  [[0, 2, 3], [2, 3, 4], [1, 1, 0]]
+    },
+    shubNiggurath: {
+      set: [[1, 3, 2], [2, 2, 4], [1, 1, 0]]
+    }
+  },
+  cards: {
+    green: greenCards,
+    brown: brownCards,
+    blue: blueCards,
+  },
+  images: [
+    // BACKGROUND
+    'background-desktop.jpg',
     // ANTIENTS
     'Azathoth.png',
     'Cthulthu.png',
     'IogSothoth.png',
     'ShubNiggurath.png',
     // CARD BACK
-    'cardBack.png'
-
-  ]
+    'cardBack.png',
+    // GREEN CARDS
+    ...greenCards.easy,
+    ...greenCards.normal,
+    ...greenCards.hard,
+    // BROWN CARDS
+    ...brownCards.easy,
+    ...brownCards.normal,
+    ...brownCards.hard,
+    // BLUE CARDS
+    ...blueCards.easy,
+    ...blueCards.normal,
+    ...blueCards.hard,
+  ],
 }
 
 export default config;
--- src/app/modules/game/index.js
+++ src/app/modules/game/index.js
@@ -1,65 +1,161 @@
 import config from "./config";
 import { qi, qs, qa } from '../helpers/helpers';
 
+let currLevel = 1;
+
+let currSet = [];
+
+let greenSet = [];
+let brownSet = [];
+let blueSet = [];
+
+let step1Set = [];
+let step2Set = [];
+let step3Set = [];
+
 const game = async () => {
   console.log("START")
   await loading();
   showAncient();
   selectAncient();
-  selectLevel()
+  selectLevel();
+  shuffleByClick();
 }
 
 // HELPERS
+function arrayShuffle(arr) {
+  const length = arr.length;
+  const res = [];
+  let tmp = arr;
+
+ for(let i = length; i > 0; i--) {
+  const randId = Math.floor(Math.random() * i);
+  const randEl = tmp[randId];
+  res.push(randEl);
+  tmp[randId] = '';
+  tmp = tmp.filter(el => el !== '')
+
+  // console.log('=================');
+  // console.log('RANDOM ID', randId);
+  // console.log('RANDOM EL', randEl);
+  // console.log('RESULT ARRAY', res)
+  // console.log('TEMP ARRAY', tmp)
+ }
+ return res;
+}
+
+function shuffleByClick() {
+  qi('shiffleCards').addEventListener('click', ()=> {
+    if(currLevel == 1) {
+
+      const greenSum = currSet[0][0] + currSet[0][1] + currSet[0][2];
+      greenSet = [...config.cards.green.easy, ...config.cards.green.normal].slice(0, greenSum);
+
+      const brownSum = currSet[1][0] + currSet[1][1] + currSet[1][2];
+      brownSet = [...config.cards.brown.easy, ...config.cards.brown.normal].slice(0, brownSum);
+
+      const blueSum = currSet[2][0] + currSet[2][1] + currSet[2][2];
+      blueSet = [...config.cards.blue.easy, ...config.cards.blue.normal].slice(0, blueSum);
+
+      console.log('greenSet', greenSet);
+      console.log('brownSet', brownSet);
+      console.log('blueSet', blueSet);
+
+      const suffledGreenSet = arrayShuffle(greenSet);
+      const suffledBrownSet = arrayShuffle(brownSet);
+      const suffledBlueSet = arrayShuffle(blueSet);
+
+      console.log('suffledGreenSet', suffledGreenSet);
+      console.log('suffledBrownSet', suffledBrownSet);
+      console.log('suffledBlueSet', suffledBlueSet);
+
+      step1Set[0] = suffledGreenSet.slice(0, currSet[0][0]);
+      step1Set[1] = suffledBrownSet.slice(0, currSet[1][0]);
+      step1Set[2] = suffledBlueSet.slice(0, currSet[2][0]);
+
+      console.log('step1Set', step1Set);
+    }
+
+    qi('step1Green').textContent = currSet[0][0];
+    qi('step2Green').textContent = currSet[0][1];
+    qi('step3Green').textContent = currSet[0][2];
+
+    qi('step1Brown').textContent = currSet[1][0];
+    qi('step2Brown').textContent = currSet[1][1];
+    qi('step3Brown').textContent = currSet[1][2];
+
+    qi('step1Blue').textContent = currSet[2][0];
+    qi('step2Blue').textContent = currSet[2][1];
+    qi('step3Blue').textContent = currSet[2][2];
+
+    // arrayShuffle([1,2,3,4,5,6,7,8,9,10])
+  })
+}
+
 function selectLevel () {
   qi('levelVeryEasy').addEventListener('click', ()=> {
     qa('.level__button').forEach(el => el.classList.remove('active'));
-    qi('levelVeryEasy').classList.add('active')
+    qi('levelVeryEasy').classList.add('active');
+    qi('shiffleCards').classList.remove('hidden');
+    currLevel = 1;
   })
   qi('levelEasy').addEventListener('click', ()=> {
     qa('.level__button').forEach(el => el.classList.remove('active'));
     qi('levelEasy').classList.add('active')
+    qi('shiffleCards').classList.remove('hidden');
+    currLevel = 2;
   })
   qi('levelNormal').addEventListener('click', ()=> {
     qa('.level__button').forEach(el => el.classList.remove('active'));
     qi('levelNormal').classList.add('active')
+    qi('shiffleCards').classList.remove('hidden');
+    currLevel = 3;
   })
   qi('levelHard').addEventListener('click', ()=> {
     qa('.level__button').forEach(el => el.classList.remove('active'));
     qi('levelHard').classList.add('active')
+    qi('shiffleCards').classList.remove('hidden');
+    currLevel = 4;
   })
   qi('levelVeryHard').addEventListener('click', ()=> {
     qa('.level__button').forEach(el => el.classList.remove('active'));
     qi('levelVeryHard').classList.add('active')
+    qi('shiffleCards').classList.remove('hidden');
+    currLevel = 5;
   })
 }
 
 function showLevels(){
-  qi('levelVeryEasy').classList.add('visible');
-  setTimeout(() => qi('levelEasy').classList.add('visible'), 300);
-  setTimeout(() => qi('levelNormal').classList.add('visible'), 600);
-  setTimeout(() => qi('levelHard').classList.add('visible'), 900)
-  setTimeout(() => qi('levelVeryHard').classList.add('visible'), 1200);
+  qi('levelVeryEasy').classList.remove('hidden');
+  setTimeout(() => qi('levelEasy').classList.remove('hidden'), 300);
+  setTimeout(() => qi('levelNormal').classList.remove('hidden'), 600);
+  setTimeout(() => qi('levelHard').classList.remove('hidden'), 900)
+  setTimeout(() => qi('levelVeryHard').classList.remove('hidden'), 1200);
 }
 
 function selectAncient() {
   qi('azathoth').addEventListener("click", () => {
     qa('.button__ancient').forEach(el => el.classList.remove('active'));
     qi('azathoth').classList.add('active');
+    currSet = config.antients.azathoth.set;
     showLevels();
   })
   qi('cthulthu').addEventListener("click", () => {
     qa('.button__ancient').forEach(el => el.classList.remove('active'));
     qi('cthulthu').classList.add('active');
+    currSet = config.antients.cthulthu.set;
     showLevels();
   })
   qi('iogSothoth').addEventListener("click", () => {
     qa('.button__ancient').forEach(el => el.classList.remove('active'));
     qi('iogSothoth').classList.add('active');
+    currSet = config.antients.iogSothoth.set;
     showLevels();
   })
   qi('shubNiggurath').addEventListener("click", () => {
     qa('.button__ancient').forEach(el => el.classList.remove('active'));
     qi('shubNiggurath').classList.add('active');
+    currSet = config.antients.shubNiggurath.set;
     showLevels();
   })
 }
@@ -73,12 +169,13 @@ function showAncient() {
 
 function loading() {
   let load = 0;
+  let step = Math.ceil(100 / config.images.length);
   return new Promise((resolve, reject) => {
     config.images.map(src => {
       const img = new Image();
       img.addEventListener('load', () => {
         setTimeout(()=> {
-          load += 2;
+          load += step;
           if(load < 100) qs('.preloader__counter').textContent = load + '%';
           if(load >= 100) {
             qs('.preloader__counter').textContent = '100%'
